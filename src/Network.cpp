c9e05bd7af0c21874a9b28968345d01aa3a176c9
arnova <arnova@eld>
2026-07-17 16:40:11 +0200

    fixed: m_bWifiConnected-flag should always be cleared in InitWifi() else we won't detect reconnects properly

--- src/Network.cpp
+++ src/Network.cpp
@@ -49,6 +49,7 @@ void CNetwork::LoadSettings()
 void CNetwork::InitWifi(const bool bReconnect)
 {
   m_wifiTimeoutTimer = 0;
+  m_bWifiConnected = false;
 
   if (bReconnect)
   {
@@ -274,7 +275,6 @@ void CNetwork::Loop()
   {
     if (m_wifiTimeoutTimer > WIFI_CONNECT_TIMEOUT)
     {
-      m_bWifiConnected = false;
       InitWifi(true);
     }
   }
