4cbc2658485c9be61429185b37d5733fdddc585b
arnova <arnova@eld>
2026-07-23 17:02:16 +0200

    changed: Report status time values in ms with 3 digit precision

--- src/PvBoilerCommandHandler.cpp
+++ src/PvBoilerCommandHandler.cpp
@@ -304,18 +304,18 @@ result_code_t CPvBoilerCommandHandler::CmdStatus(const char *strArgs)
   CTermPrint::print("%");
 
   CTermPrint::print(" net_period=");
-  CTermPrint::print(String(static_cast<float>(m_pvBoiler.GetNetPeriod()) / 1000.0f, 2));
+  CTermPrint::print(String(static_cast<float>(m_pvBoiler.GetNetPeriod()) / 1000.0f, 3));
   CTermPrint::print("ms");
 
   CTermPrint::print(" zero_cross_window=");
-  CTermPrint::print(String(m_pvBoiler.GetZeroCrossWindow()));
-  CTermPrint::print("us");
+  CTermPrint::print(String(static_cast<float>(m_pvBoiler.GetZeroCrossWindow()) / 1000.0f, 3));
+  CTermPrint::print("ms");
 
   if (m_pvBoiler.GetDimStyle() == CPvBoiler::DIM_STYLE_PHASE_ANGLE)
   {
     CTermPrint::print(" phase_angle=");
-    CTermPrint::print(String(m_pvBoiler.GetTriacPhaseAngle()));
-    CTermPrint::print("us");
+    CTermPrint::print(String(static_cast<float>(m_pvBoiler.GetTriacPhaseAngle()) / 1000.0f, 3));
+    CTermPrint::print("ms");
 
     CTermPrint::print(" angle_factor=");
     CTermPrint::print(String(m_pvBoiler.GetTriacAngleFactor(), 4));
