3d2d13c13e8d00efeab2cf320ecfb811933fefd2
arnova <arnova@eld>
2026-07-27 17:04:36 +0200

    fixed: Web url in main.cpp

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -2,7 +2,7 @@
   ESP PV-Boiler - ESP Controlled PV Boiler
   Last update: July 27, 2026
   (C) Copyright 2026 by Arno van Amersfoort
-  Web                   : https://github.com/arnova/ctrl4dkn
+  Web                   : https://github.com/arnova/pvboiler
   Email                 : a r n o DOT v a n DOT a m e r s f o o r t AT g m a i l DOT c o m
                           (note: you must remove all spaces and substitute the @ and the . at the proper locations!)
   ----------------------------------------------------------------------------------------------------------------------
