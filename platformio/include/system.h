ab0f9722b3d85f5b85433044dad25a1f05ba5964
arnova <arnova@eld>
2026-07-27 19:23:49 +0200

    changed: Version bump to v1.01

--- platformio/include/system.h
+++ platformio/include/system.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 
 // Version string:
-#define MY_VERSION "1.00"
+#define MY_VERSION "1.01"
 
 // Firmware version string
 const char VER_STR_P[] PROGMEM = "PvBoiler " MY_VERSION " - (C) 2026 Arno van Amersfoort (Arnova)";
